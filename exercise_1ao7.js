b5da56e6ccb7e1f659df2e0beef7694742c0a590
r0ger <rogeriomaiajr13@gmail.com>
2021-05-20 17:42:18 -0300

    Resolucao do exercicio 3

--- exercise_1ao7.js
+++ exercise_1ao7.js
@@ -1,8 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
+let average = 0;
 
 for(let index = 0; index < numbers.length; index += 1){
     sum = sum + numbers[index];
 }
 
-console.log(sum);
+average = sum / numbers.length;
+console.log(average);
