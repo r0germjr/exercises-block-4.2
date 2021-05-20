892900bb6a0c87d3c1718b9ef51cab851472906f
r0ger <rogeriomaiajr13@gmail.com>
2021-05-20 18:12:59 -0300

    Resoluçao do exercicio 8

--- exercise_1ao7.js
+++ exercise_1ao7.js
@@ -1,10 +1,7 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let lowestNumber = numbers[0];
+let numbers = [];
 
-for(index = 0; index < numbers.length; index += 1) {
-    if(numbers[index] < lowestNumber) {
-        lowestNumber = numbers[index];
-    }
+for (let index = 0; index < 25; index += 1) {
+    numbers[index] = index + 1;
 }
 
-console.log(lowestNumber);
+console.log(numbers);
